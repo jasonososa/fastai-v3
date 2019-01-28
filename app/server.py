de899f8ce4cc18ba56686cf8edfc1017db79892d
Jason Sosa <29934731+jasonososa@users.noreply.github.com>
2019-01-27 17:02:50 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=17ButfQ68Zd4i7lFq54roaELMrMV8s6bc'
 export_file_name = 'export.pkl'
 
 classes = ['class-1', 'class-2', 'class-3']
