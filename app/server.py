28e67f335385a214b0d0e7c706fc7efbde04bca4
Jason Sosa <29934731+jasonososa@users.noreply.github.com>
2019-01-27 22:08:38 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -27,7 +27,7 @@ async def download_file(url, dest):
 
 async def setup_learner():
     await download_file(export_file_url, path/export_file_name)
-    learn = load_learner(path, export_file_name, map_location="cpu")
+    learn = load_learner(path, export_file_name, cpu=True)
     return learn
 
 loop = asyncio.get_event_loop()
